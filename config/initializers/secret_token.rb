749b116eb375205a349abe27b9150e4c0b63d9bf
Francis Heyzer <M1utter1>
2013-08-30 01:13:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinteyzer::Application.config.secret_token = '0bcfa46e0026cd1186463df2c4d8707e306294068a62404015313bb7d1a62ba8def445c9d88c014bbfa4b04bd37829db3b43c0f161cea1631f59075924f4d29a'
